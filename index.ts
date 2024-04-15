74e52b4cfaa206d167daad9336bf081e10db7abd
Askhad Arzybaev <aarzybaev@gmail.com>
2024-04-15 20:33:50 +0600

    created index file

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from "mongoose";
+
+import artistsRouter from "./routers/artists";
+import albumsRouter from "./routers/albums";
+import tracksRouter from "./routers/tracks";
+import config from "./config";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use('/artists', artistsRouter);
+app.use('/albums', albumsRouter);
+app.use('/tracks', tracksRouter);
+
+const run = async () => {
+    await mongoose.connect(config.mongoose.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+};
+
+void run();
